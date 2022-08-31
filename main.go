3b3818d4db64803a25e0a6e57c2e5df1275a9592
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2022-08-30 21:52:50 -0600

    started db connection

--- main.go
+++ main.go
@@ -1,12 +1,81 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/jonathanruiz/iptracker-app/cmd"
+	_ "github.com/lib/pq"
 )
 
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
 func main() {
+
+	app := fiber.New()
+
+	app.Get("/", indexHandler)
+
+	app.Post("/", postHandler)
+
+	app.Put("/update", putHandler)
+
+	app.Delete("/delete", deleteHandler)
+
+	// assign environment variables to db variables
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	dbname := os.Getenv("DBNAME")
+
+	// Create connection string
+	connStr := "postgresql://" + user + ":" + password + "@" + host + "/" + dbname + "?sslmode=disable"
+
+	// Connect to database
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return indexHandler(c, db)
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		return postHandler(c, db)
+	})
+
+	app.Put("/update", func(c *fiber.Ctx) error {
+		return putHandler(c, db)
+	})
+
+	app.Delete("/delete", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
+	})
+
+	if port == "" {
+		port = "3000"
+	}
+	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
+
 	fmt.Println("Private IP : ", cmd.GetPrivateIP())
 	fmt.Println("Public IP: ", cmd.GetPublicIP())
 }
