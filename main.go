3b410365ef734adceef713dd3587e1c3ca1e2109
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2022-08-31 22:05:45 -0600

    connection is refusing for some reason

--- main.go
+++ main.go
@@ -7,47 +7,55 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+	"github.com/joho/godotenv"
 	"github.com/jonathanruiz/iptracker-app/cmd"
 	_ "github.com/lib/pq"
 )
 
 func indexHandler(c *fiber.Ctx, db *sql.DB) error {
-	return c.SendString("Hello")
+	var res string
+	var ips []string
+	rows, err := db.Query("SELECT * FROM ip")
+	fmt.Println("rows: ", rows)
+
+	defer rows.Close()
+
+	if err != nil {
+		log.Fatalln(err)
+		c.JSON("An error occured")
+	}
+	for rows.Next() {
+		rows.Scan(&res)
+		ips = append(ips, res)
+	}
+	return c.Render("index", fiber.Map{
+		"Todos": ips,
+	})
 }
 
 func postHandler(c *fiber.Ctx, db *sql.DB) error {
 	return c.SendString("Hello")
 }
 
-func putHandler(c *fiber.Ctx, db *sql.DB) error {
-	return c.SendString("Hello")
-}
-
-func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
-	return c.SendString("Hello")
-}
-
 func main() {
 
-	app := fiber.New()
-
-	app.Get("/", indexHandler)
-
-	app.Post("/", postHandler)
-
-	app.Put("/update", putHandler)
-
-	app.Delete("/delete", deleteHandler)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	// assign environment variables to db variables
 	host := os.Getenv("HOST")
-	port := os.Getenv("PORT")
-	user := os.Getenv("USER")
+	web_port := os.Getenv("PORT")
+	user := os.Getenv("USERNAME")
 	password := os.Getenv("PASSWORD")
 	dbname := os.Getenv("DBNAME")
 
 	// Create connection string
 	connStr := "postgresql://" + user + ":" + password + "@" + host + "/" + dbname + "?sslmode=disable"
+
+	fmt.Println(connStr)
 
 	// Connect to database
 	db, err := sql.Open("postgres", connStr)
@@ -55,7 +63,14 @@ func main() {
 		log.Fatal(err)
 	}
 
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
 	app.Get("/", func(c *fiber.Ctx) error {
+		println("Is this working?")
 		return indexHandler(c, db)
 	})
 
@@ -63,18 +78,13 @@ func main() {
 		return postHandler(c, db)
 	})
 
-	app.Put("/update", func(c *fiber.Ctx) error {
-		return putHandler(c, db)
-	})
-
-	app.Delete("/delete", func(c *fiber.Ctx) error {
-		return deleteHandler(c, db)
-	})
-
-	if port == "" {
-		port = "3000"
+	if web_port == "" {
+		web_port = "3000"
 	}
-	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
+
+	app.Static("/", "./public") // add this before starting the app
+
+	log.Fatalln(app.Listen(fmt.Sprintf(":%v", web_port)))
 
 	fmt.Println("Private IP : ", cmd.GetPrivateIP())
 	fmt.Println("Public IP: ", cmd.GetPublicIP())
